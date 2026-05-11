1feb56b96eabe2ca6d10055f95ef948d111ffe84
Om Rathore <omrathore9717@gmail.com>
2026-05-11 21:29:00 +0530

    fixed js completely

--- script.js
+++ script.js
@@ -1,77 +1,73 @@
-const themeToggle = document.getElementById("themeToggle");
+const themeToggle =
+document.getElementById("themeToggle");
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+if (themeToggle) {
 
-  if (document.body.classList.contains("light-mode")) {
-    themeToggle.innerHTML = "☀️";
-  } else {
-    themeToggle.innerHTML = "🌙";
-  }
-});
+  themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+    if (
+      document.body.classList.contains(
+        "light-mode"
+      )
+    ) {
+
+      themeToggle.innerHTML = "☀️";
+
+    } else {
+
+      themeToggle.innerHTML = "🌙";
+
+    }
+
+  });
+
+}
 
 
-// LIVE MARKET DATA
 
-async function loadMarketData() {
+// MARKET TEXT
 
-  // NIFTY
+function loadMarketData() {
 
-  document.getElementById("nifty").innerHTML =
+  const nifty =
+  document.getElementById("nifty");
+
+  const sensex =
+  document.getElementById("sensex");
+
+  const gold =
+  document.getElementById("gold");
+
+  const usd =
+  document.getElementById("usd");
+
+  const bitcoin =
+  document.getElementById("bitcoin");
+
+  if (nifty)
+    nifty.innerHTML =
     "📈 NIFTY 50 : 24,850 ▲ +125";
 
-  // SENSEX
-
-  document.getElementById("sensex").innerHTML =
+  if (sensex)
+    sensex.innerHTML =
     "💰 SENSEX : 81,450 ▲ +410";
 
-  // GOLD
-
-  document.getElementById("gold").innerHTML =
+  if (gold)
+    gold.innerHTML =
     "🥇 GOLD : ₹96,200";
 
-  // USD INR
-
-  document.getElementById("usd").innerHTML =
+  if (usd)
+    usd.innerHTML =
     "💵 USD/INR : ₹83.45";
 
-  // BITCOIN
-
-  document.getElementById("bitcoin").innerHTML =
+  if (bitcoin)
+    bitcoin.innerHTML =
     "🪙 BITCOIN : $104,000";
 
 }
 
 loadMarketData();
 
-
-// AUTO REFRESH EVERY 10 SECONDS
-
 setInterval(loadMarketData, 10000);
-
-
-
-// CONTACT FORM
-
-
-
-// SCROLL ANIMATION
-
-const sections = document.querySelectorAll("section");
-
-window.addEventListener("scroll", () => {
-
-  sections.forEach((section) => {
-
-    const sectionTop =
-      section.getBoundingClientRect().top;
-
-    if (sectionTop < window.innerHeight - 100) {
-
-      section.classList.add("show");
-
-    }
-
-  });
-
-});

45acce34fab191f26621d1f20d9e3a92de8b8b68
Om Rathore <omrathore9717@gmail.com>
2026-05-11 19:38:28 +0530

    premium website completed

--- script.js
+++ script.js
@@ -1,3 +1,84 @@
-function showMessage(){
-  alert("Welcome to OM Financial Service");
+/* SCROLL ANIMATION */
+
+const revealElements = document.querySelectorAll(
+'.card, .testimonial-card, .stat-box'
+);
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+
+  revealElements.forEach(element => {
+
+    const windowHeight = window.innerHeight;
+
+    const revealTop =
+    element.getBoundingClientRect().top;
+
+    const revealPoint = 100;
+
+    if(revealTop < windowHeight - revealPoint){
+
+      element.classList.add('active');
+
+    }
+
+  });
+
 }
+
+reveal();
+
+/* COUNTER ANIMATION */
+
+const counters =
+document.querySelectorAll('.stat-box h2');
+
+counters.forEach(counter => {
+
+  const updateCounter = () => {
+
+    const target =
+    +counter.innerText.replace('+','');
+
+    const current =
+    +counter.getAttribute('data-count') || 0;
+
+    const increment = target / 100;
+
+    if(current < target){
+
+      const value =
+      Math.ceil(current + increment);
+
+      counter.innerText = value + "+";
+
+      counter.setAttribute('data-count', value);
+
+      setTimeout(updateCounter, 30);
+
+    }else{
+
+      counter.innerText = target + "+";
+
+    }
+
+  };
+
+  updateCounter();
+
+});
+
+/* NAVBAR SHADOW */
+
+window.addEventListener('scroll', () => {
+
+  const header =
+  document.querySelector('header');
+
+  header.classList.toggle(
+    'sticky',
+    window.scrollY > 50
+  );
+
+});
